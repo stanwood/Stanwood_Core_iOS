8fcda121601c5b59ef4febc1d254ba9aca4247fa
Tal Zion <talezion@gmail.com>
2018-02-26 12:21:17 +0200

    Loadable refactoring, setting static and nonstatic options

--- StanwoodCore/Classes/Loadable.swift
+++ StanwoodCore/Classes/Loadable.swift
@@ -16,13 +16,26 @@ extension UIView: Loadable { }
 public extension Loadable where Self: UIView {
     
     /**
-     load `UIView` from nib
+     load `UIView` from xib
      
      - Parameters:
         - frame: CGRect default = nil
         - bundle: default = Bundle.main
      */
-    public func loadFromNib(withFrame frame: CGRect? = nil, bundle: Bundle = Bundle.main) -> Self? {
+    public static func loadFromXib(withFrame frame: CGRect? = nil, bundle: Bundle = Bundle.main) -> Self? {
+        guard let view = bundle.loadNibNamed(staticIdentifier, owner: nil, options: nil)?.last as? Self else { return nil }
+        view.frame = frame ?? view.frame
+        return view
+    }
+    
+    /**
+     load `UIView` from outlet
+     
+     - Parameters:
+        - bundle: default = Bundle.main
+     */
+    @discardableResult
+    public func loadFromOutlet(bundle: Bundle = Bundle.main) -> Self? {
         guard let view = bundle.loadNibNamed(identifier, owner: nil, options: nil)?.last as? Self else { return nil }
         self.addSubview(view)
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -30,8 +43,14 @@ public extension Loadable where Self: UIView {
         return view
     }
 
-    var identifier: String {
+    /***/
+    private static var staticIdentifier: String {
         return String(describing: self)
+    }
+    
+    /***/
+    private var identifier: String {
+        return String(describing: type(of: self))
     }
 }
 
